b8f9fe8262c9433e05dbd0d5f42172da8c545720
Jack <relee6203@gmail.com>
2024-11-13 00:15:40 +0900

    test: 기존 신규 작성 테스트 수정

--- src/__tests__/new.integration.spec.tsx
+++ src/__tests__/new.integration.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor, within } from '@testing-library/react';
+import { act, logRoles, render, screen, waitFor, within } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import React from 'react';
 
@@ -24,18 +24,20 @@ const initialEvents = [
   },
 ] as Event[];
 
-const renderComponent = () =>
+const renderComponent = () => {
   render(
     <Provider>
       <App />
     </Provider>
   );
+};
 
-describe.only('반복 일정 테스트', () => {
-  beforeEach(() => {
-    renderComponent();
+describe('반복 일정 테스트', () => {
+  afterEach(() => {
+    vi.useRealTimers();
   });
-  const getElement = () => {
+
+  const saveEvent = async (newEvent: EventForm) => {
     const titleInput = screen.getByLabelText('제목');
     const dateInput = screen.getByLabelText('날짜');
     const startTimeInput = screen.getByLabelText('시작 시간');
@@ -43,7 +45,6 @@ describe.only('반복 일정 테스트', () => {
     const descriptionInput = screen.getByLabelText('설명');
     const locationInput = screen.getByLabelText('위치');
     const categoryInput = screen.getByLabelText('카테고리');
-    const repeatCheckbox = screen.getByLabelText('반복 일정');
     const notificationTimeSelect = screen.getByLabelText('알림 설정');
 
     // 반복 일정 체크 시 추가되는 요소들
@@ -52,37 +53,6 @@ describe.only('반복 일정 테스트', () => {
 
     // 일정 추가 버튼
     const addEventButton = screen.getByTestId('event-submit-button');
-    return {
-      titleInput,
-      dateInput,
-      startTimeInput,
-      endTimeInput,
-      descriptionInput,
-      locationInput,
-      categoryInput,
-      repeatCheckbox,
-      notificationTimeSelect,
-      repeatTypeSelect,
-      repeatIntervalInput,
-      addEventButton,
-    };
-  };
-
-  const saveEvent = async (els: ReturnType<typeof getElement>, newEvent: EventForm) => {
-    const {
-      titleInput,
-      dateInput,
-      startTimeInput,
-      endTimeInput,
-      descriptionInput,
-      locationInput,
-      categoryInput,
-      notificationTimeSelect,
-      repeatCheckbox,
-      repeatTypeSelect,
-      repeatIntervalInput,
-      addEventButton,
-    } = els;
     // 제목을 지우고 수정된 제목 입력
     await userEvent.clear(titleInput);
     await userEvent.type(titleInput, newEvent.title);
@@ -113,84 +83,80 @@ describe.only('반복 일정 테스트', () => {
     // 알림 시간을 수정된 알림 시간 입력
     await userEvent.selectOptions(notificationTimeSelect, newEvent.notificationTime.toString());
 
-    // 반복 일정 체크 시 반복 유형과 반복 간격 설정
-
-    await userEvent.click(repeatCheckbox);
-
     if (repeatTypeSelect) {
       await userEvent.selectOptions(repeatTypeSelect, newEvent.repeat.type);
     }
     if (repeatIntervalInput) {
+      await userEvent.clear(repeatIntervalInput);
       await userEvent.type(repeatIntervalInput, newEvent.repeat.interval.toString());
     }
-
     // 일정 추가
     await userEvent.click(addEventButton);
+  };
+
+  const diffDate = (date1: Date, date2: Date) => {
+    return date1.getTime() - date2.getTime();
+  };
+
+  const navigate = async (from: Date, to: Date) => {
+    const fromDate = new Date(from);
+    const toDate = new Date(to);
+
+    const nextButton = screen.getByLabelText('Next');
+    const prevButton = screen.getByLabelText('Previous');
+
+    const fromMonth = new Date(fromDate.getFullYear(), fromDate.getMonth(), 1);
+    const toMonth = new Date(toDate.getFullYear(), toDate.getMonth(), 1);
+
+    const yearGap = toMonth.getFullYear() - fromMonth.getFullYear();
+    const monthGap = toMonth.getMonth() - fromMonth.getMonth();
+
+    let totalMove = yearGap * 12 + monthGap;
+    for (let i = 0; i < totalMove; i++) {
+      if (diffDate(fromDate, toDate) < 0) {
+        await userEvent.click(nextButton);
+      } else {
+        await userEvent.click(prevButton);
+      }
+    }
+    vi.setSystemTime(fromDate);
   };
 
   const dailyEvent = {
     type: 'daily',
     name: '2일간 반복 일정',
     interval: 2,
-    expectDate: ['2024-10-15', '2024-10-17', '2024-10-19'],
+    expectDate: ['2024-10-15', '2024-10-17'],
   };
 
   const weeklyEvent = {
     type: 'weekly',
     name: '2주간 반복 일정',
     interval: 2,
-    expectDate: ['2024-10-15', '2024-10-22', '2024-10-29'],
+    expectDate: ['2024-10-15', '2024-10-29'],
   };
 
   const monthlyEvent = {
     type: 'monthly',
     name: '2달간 반복 일정',
     interval: 2,
-    expectDate: ['2024-10-15', '2024-12-15', '2025-02-15'],
+    expectDate: ['2024-10-15', '2024-12-15'],
   };
 
   const yearlyEvent = {
     type: 'yearly',
     name: '2년간 반복 일정',
     interval: 2,
-    expectDate: ['2024-10-15', '2026-10-15', '2028-10-15'],
+    expectDate: ['2024-10-15', '2026-10-15'],
   };
 
-  it.only('반복 일정 적용 시 캘린더에 아이콘이 노출된다.', async () => {
-    const _initialEvents = [...initialEvents];
-    setupCreateHandler(_initialEvents);
-
-    const newEvent = {
-      title: '새로운 일정',
-      date: '2024-10-15',
-      startTime: '10:00',
-      endTime: '11:00',
-      description: '새로운 일정 설명',
-      location: '새로운 장소',
-      category: '업무',
-      notificationTime: 10,
-      repeat: { type: 'daily', interval: 10 },
-    } as EventForm;
-
-    const els = getElement();
-
-    await saveEvent(els, newEvent);
-
-    await screen.findAllByText('일정이 추가되었습니다.');
-
-    const calendarView = screen.getByTestId('month-view');
-
-    screen.debug(within(calendarView).getAllByText(/반복 일정 캘린더/i));
-
-    await waitFor(() => {
-      expect(within(calendarView).getAllByText('반복 일정 캘린더')).toHaveLength(8);
-    });
-  });
-  test.skip.each([dailyEvent, weeklyEvent, monthlyEvent, yearlyEvent])(
+  it.only.each([dailyEvent, weeklyEvent, monthlyEvent, yearlyEvent])(
     '$name 반복 일정 생성 시 리스트에 정확히 노출된다.',
     async ({ type, interval, expectDate }) => {
       const _initialEvents = [...initialEvents];
       setupCreateHandler(_initialEvents);
+
+      renderComponent();
 
       const newEvent = {
         title: '새로운 일정',
@@ -204,27 +170,59 @@ describe.only('반복 일정 테스트', () => {
         repeat: { type, interval },
       } as EventForm;
 
-      const els = getElement();
+      await act(async () => {
+        await saveEvent(newEvent);
+      });
 
-      await saveEvent(els, newEvent);
+      for (let i = 0; i < expectDate.length; i++) {
+        const prevDate = i === 0 ? new Date() : new Date(expectDate[i - 1]);
+        await navigate(prevDate, new Date(expectDate[i]));
 
-      const eventList = screen.getByTestId('event-list');
-
-      if (type === 'daily' || type === 'weekly') {
-        expectDate.forEach((date) => {
-          expect(within(eventList).getAllByText(date)[0]).toBeInTheDocument();
-        });
-      }
-
-      //TODO: 월간 반복 일정 테스트
-      if (type === 'monthly') {
-        expect(within(eventList).getAllByText(expectDate[0])[0]).toBeInTheDocument();
-      }
-
-      //TODO: 연간 반복 일정 테스트
-      if (type === 'yearly') {
-        expect(within(eventList).getAllByText(expectDate[0])[0]).toBeInTheDocument();
+        const calendarView = screen.getByTestId('month-view');
+        expect(
+          within(calendarView).getByText(`반복 일정 캘린더 ${expectDate[i]}`)
+        ).toBeInTheDocument();
       }
     }
   );
+
+  it('반복 일정 적용 시 캘린더에 아이콘이 노출된다.', async () => {
+    const _initialEvents = [...initialEvents];
+    setupCreateHandler(_initialEvents);
+
+    renderComponent();
+
+    const newEvent = {
+      title: '새로운 일정',
+      date: '2024-10-15',
+      startTime: '10:00',
+      endTime: '11:00',
+      description: '새로운 일정 설명',
+      location: '새로운 장소',
+      category: '업무',
+      notificationTime: 10,
+      repeat: { type: 'daily', interval: 2 },
+    } as EventForm;
+
+    await act(async () => {
+      await saveEvent(newEvent);
+    });
+
+    const calendarView = screen.getByTestId('month-view');
+
+    await screen.findAllByText(/일정이 추가되었습니다./i);
+
+    expect(within(calendarView).getAllByText(/반복 일정 캘린더/i)).toHaveLength(9);
+
+    // 날짜별로 확인 잘 들어갔는지 확인
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-15')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-17')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-19')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-21')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-23')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-25')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-27')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-29')).toBeInTheDocument();
+    expect(within(calendarView).getByText('반복 일정 캘린더 2024-10-31')).toBeInTheDocument();
+  });
 });

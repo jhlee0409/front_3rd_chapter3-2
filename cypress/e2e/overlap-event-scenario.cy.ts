d02b49f10601afa7f5a2fefb07e89b55530dc71b
Jack <relee6203@gmail.com>
2024-11-14 23:08:20 +0900

    test: cypress - 겹치는 일정에 대한 시나리오

---
+++ cypress/e2e/overlap-event-scenario.cy.ts
@@ -0,0 +1,72 @@
+before(() => {
+  cy.task('resetDb');
+});
+
+describe('겹치는 일정 추가에 대한 시나리오 테스트', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.task('resetDb');
+    // 이전 테스트에서 추가한 일정을 미리 추가하여 경고창을 띄우는 상태를 유지
+    cy.findByText('일정 로딩 완료!').should('be.visible');
+
+    cy.addEvent({
+      title: '일정 한번 추가해보자',
+      date: '2024-11-13',
+      startTime: '10:11',
+      endTime: '11:11',
+      description: '일정 한번 추가해보자 설명',
+      location: '일정 한번 추가해보자 위치',
+      category: '가족',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    });
+  });
+
+  it('일정을 추가합니다.', () => {
+    cy.checkEvent('month', '일정 한번 추가해보자', 'visible');
+  });
+
+  it('시간이 겹치면 뜨는 경고창에서 취소를 누르면 경고창이 사라집니다.', () => {
+    cy.addEvent({
+      title: '일정 한번 추가해보자',
+      date: '2024-11-13',
+      startTime: '10:30',
+      endTime: '11:30',
+      description: '일정 한번 추가해보자 설명',
+      location: '일정 한번 추가해보자 위치',
+      category: '가족',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    });
+    cy.findByText('일정 겹침 경고').should('be.visible');
+    cy.findByText(/2024-11-13 10:11-11:11/i).should('be.visible');
+
+    cy.findByText('일정 겹침 경고')
+      .parent()
+      .within(() => cy.findByText('취소').click());
+
+    cy.findByText('일정 겹침 경고').should('not.exist');
+  });
+  it('시간이 겹치면 뜨는 경고창에서 확인을 누르면 일정이 추가됩니다.', () => {
+    cy.addEvent({
+      title: '일정 한번 추가해보자',
+      date: '2024-11-13',
+      startTime: '10:30',
+      endTime: '11:30',
+      description: '일정 한번 추가해보자 설명',
+      location: '일정 한번 추가해보자 위치',
+      category: '가족',
+      repeat: { type: 'none', interval: 0 },
+      notificationTime: 10,
+    });
+    cy.findByText('일정 겹침 경고').should('be.visible');
+    cy.findByText(/2024-11-13 10:11-11:11/i).should('be.visible');
+
+    cy.findByText('일정 겹침 경고')
+      .parent()
+      .within(() => cy.findByText('계속 진행').click());
+
+    cy.findByText('일정 겹침 경고').should('not.exist');
+    cy.checkEvent('month', '일정 한번 추가해보자', 'visible', 2);
+  });
+});

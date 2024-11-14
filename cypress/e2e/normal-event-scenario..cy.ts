95db28bf88de85365f8c7a0481229daf6c310abd
Jack <relee6203@gmail.com>
2024-11-14 22:41:32 +0900

    test: cypress - 일정 생성시 검색 결과에도 제대로 반영 체크

--- cypress/e2e/normal-event-scenario..cy.ts
+++ cypress/e2e/normal-event-scenario..cy.ts
@@ -13,16 +13,23 @@ describe('일반적인 일정 시나리오 테스트', () => {
     cy.addEvent({
       title: '일정 한번 추가해보자',
       date: '2024-11-13',
-      startTime: '10:00',
-      endTime: '11:00',
+      startTime: '10:11',
+      endTime: '11:11',
       description: '일정 한번 추가해보자 설명',
       location: '일정 한번 추가해보자 위치',
-      category: '업무',
+      category: '가족',
       repeat: { type: 'none', interval: 0 },
       notificationTime: 10,
     });
 
     cy.checkEvent('month', '일정 한번 추가해보자', true);
+    cy.findSearchedEvent(/2024-11-13/i).should('be.visible');
+    cy.findSearchedEvent(/10:11/i).should('be.visible');
+    cy.findSearchedEvent(/11:11/i).should('be.visible');
+    cy.findSearchedEvent(/일정 한번 추가해보자 설명/i).should('be.visible');
+    cy.findSearchedEvent(/일정 한번 추가해보자 위치/i).should('be.visible');
+    cy.findSearchedEvent(/가족/i).should('be.visible');
+    cy.findSearchedEvent(/알림: 10분 전/i).should('be.visible');
   });
   it('추가된 일정을 수정합니다.', () => {
     cy.findSearchedEvent('일정 한번 추가해보자').findByLabelText('Edit event').click();
